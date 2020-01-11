1f667658e7f7a7e293fd73226766d28ad7b38289
sushruthj <susruthanvesh@gmail.com>
2020-01-11 16:34:47 +0530

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -158,7 +158,7 @@ def command_default(m):
     
     decodeval=(decode(Image.open('xyz.jpeg')))
     bardata=decodeval[0].data
-    #print bardata
+    print bardata
     #isbndata=editions(bardata, service='merge')
     
     
